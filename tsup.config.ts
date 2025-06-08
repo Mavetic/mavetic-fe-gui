0282c9aeabc0234780989080de6d93447084a071
marek.vele <marek.vele@codx.cz>
2025-06-08 10:40:45 +0200

    Fix exporting build files

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,17 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   target: "ES2020",
-  external: ["react"],
+  external: [
+    "react",
+    "@emotion/react",
+    "@emotion/styled",
+    "@mui/material",
+    "@mui/icons-material",
+  ],
   splitting: false,
   treeshake: false,
   dts: true,
-  format: ["esm"],
+  format: ["esm", "cjs"],
   esbuildOptions: (options) => {
     options.banner = {
       js: '"use client";',
